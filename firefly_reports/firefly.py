cb69b22bfcefb2aaae5ade7131174c65c57d3571
Christopher Tyler <christophertyler@engineer.com>
2025-05-20 20:05:32 -0500

    WIP: Initial API calls should be done.

--- firefly_reports/firefly.py
+++ firefly_reports/firefly.py
@@ -1,6 +1,8 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Dict, Any, List
 import requests
+import datetime
+import re
 
 @dataclass
 class Firefly:
@@ -30,3 +32,56 @@ class Firefly:
             categories = session.get(categories_url).json()["data"]
 
         return categories
+
+    def category_report(self,start_date: datetime.date, end_date: datetime.date) -> List[Dict[str, float]]:
+        header = {"Authorization": f"Bearer {self.access_token}"}
+        categories_url = f"{self.url}/api/v1/categories"
+
+        with requests.Session() as session:
+            session.headers.update(header)
+            totals = list()
+
+            for category in self.get_categories():
+                url = f"{categories_url}/{category['id']}?start={start_date}&end={end_date}"
+                category_data = session.get(url).json()["data"]
+                category_name = category_data["attributes"]["name"]
+                try:
+                    category_spent = category_data["attributes"]["spent"][0]["sum"]
+                except (KeyError, IndexError):
+                    category_spent = 0
+                try:
+                    category_earned = category_data["attributes"]["earned"][0]["sum"]
+                except (KeyError, IndexError):
+                    category_earned = 0
+                category_total = float(category_spent) + float(category_earned)
+                totals.append(
+                    {
+                        "name": category_name,
+                        "spent": category_spent,
+                        "earned": category_earned,
+                        "total": category_total,
+                    }
+                )
+
+        return totals
+
+    def summary_report(self,start_date: datetime.date, end_date: datetime.date) -> Dict[str, float]:
+        header = {"Authorization": f"Bearer {self.access_token}"}
+        summary_url = f"{self.url}/api/v1/summary/basic?start={start_date}&end={end_date}"
+
+        with requests.Session() as session:
+            session.headers.update(header)
+            summary = session.get(summary_url).json()
+
+        for key in summary:
+            if re.match(r"spent-in-.*", key):
+                currency_name = key.replace("spent-in-", "")
+        spent = summary["spent-in-" + currency_name]["monetary_value"]
+        earned = summary["earned-in-" + currency_name]["monetary_value"]
+        net_change = summary["balance-in-" + currency_name]["monetary_value"]
+
+        return {
+            "spent": spent,
+            "earned": earned,
+            "net_change": net_change,
+        }
